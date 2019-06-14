16130e5ef3c0bfbfcdb0b09c5fb182086e2a9093
梁山广 <1648266192@qq.com>
2019-06-14 15:03:11 +0800

    Create ConsumerApplication.java

---
+++ RabbitMQ/RabbitMQ消息中间件极速入门与实战/第2章_整合SpringBoot2/springboot-rabbitmq/consumer/src/main/java/com/huawei/l00379880/ConsumerApplication.java
@@ -0,0 +1,16 @@
+package com.huawei.l00379880;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 梁山广
+ */
+@SpringBootApplication
+public class ConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerApplication.class, args);
+    }
+
+}
