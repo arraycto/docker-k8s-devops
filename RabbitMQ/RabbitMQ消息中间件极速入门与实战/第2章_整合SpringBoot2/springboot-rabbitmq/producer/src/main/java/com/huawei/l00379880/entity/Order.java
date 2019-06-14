866e8ea4946976a1833b221865739d34def13570
梁山广 <1648266192@qq.com>
2019-06-14 14:54:01 +0800

    Create Order.java

---
+++ RabbitMQ/RabbitMQ消息中间件极速入门与实战/第2章_整合SpringBoot2/springboot-rabbitmq/producer/src/main/java/com/huawei/l00379880/entity/Order.java
@@ -0,0 +1,22 @@
+package com.huawei.l00379880.entity;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/***********************************************************
+ * @note      : 订单实体类
+ * @author    : 梁山广
+ * @version   : V1.0 at 2019/6/14 10:26
+ ***********************************************************/
+@Data
+public class Order implements Serializable {
+
+    private static final long serialVersionUID = -2724110603750721693L;
+    private String id;
+    private String name;
+    /**
+     * 存储消息发送的唯一标志
+     */
+    private String messageId;
+}
