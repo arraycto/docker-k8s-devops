93e1630f7dca73bf604c599fa79bfcd14c9dcbd4
梁山广 <1648266192@qq.com>
2019-06-14 14:53:06 +0800

    Create ProducerApplication.java

---
+++ RabbitMQ/RabbitMQ消息中间件极速入门与实战/第2章_整合SpringBoot2/springboot-rabbitmq/producer/src/main/java/com/huawei/l00379880/ProducerApplication.java
@@ -0,0 +1,16 @@
+package com.huawei.l00379880;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 梁山广
+ */
+@SpringBootApplication
+public class ProducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProducerApplication.class, args);
+    }
+
+}
