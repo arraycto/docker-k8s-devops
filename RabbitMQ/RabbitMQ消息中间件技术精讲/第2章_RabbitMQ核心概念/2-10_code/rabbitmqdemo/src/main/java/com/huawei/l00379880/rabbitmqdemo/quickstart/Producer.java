a37462fce04847804dbe892888e8231fb948d718
liang shan guang <1648266192@qq.com>
2019-07-09 08:21:55 +0800

    demo提交

---
+++ RabbitMQ/RabbitMQ消息中间件技术精讲/第2章_RabbitMQ核心概念/2-10_code/rabbitmqdemo/src/main/java/com/huawei/l00379880/rabbitmqdemo/quickstart/Producer.java
@@ -0,0 +1,10 @@
+/***********************************************************
+ * @Description : 生产者
+ * @author      : 梁山广(Laing Shan Guang)
+ * @date        : 2019-07-09 08:21
+ * @email       : liangshanguang2@gmail.com
+ ***********************************************************/
+package com.huawei.l00379880.rabbitmqdemo.quickstart;
+
+public class Producer {
+}
