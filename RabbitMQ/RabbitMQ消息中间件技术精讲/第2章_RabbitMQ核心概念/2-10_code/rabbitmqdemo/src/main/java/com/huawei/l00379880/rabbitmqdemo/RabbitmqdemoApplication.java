4ad1edc259dee43fe86fb555d8d43da1d1dc7a3d
liang shan guang <1648266192@qq.com>
2019-07-09 08:16:24 +0800

    RabbitMQ的demo项目

---
+++ RabbitMQ/RabbitMQ消息中间件技术精讲/第2章_RabbitMQ核心概念/2-10_code/rabbitmqdemo/src/main/java/com/huawei/l00379880/rabbitmqdemo/RabbitmqdemoApplication.java
@@ -0,0 +1,13 @@
+package com.huawei.l00379880.rabbitmqdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitmqdemoApplication.class, args);
+    }
+
+}
