4df1531c61293a7059ee1e128db412b20d4562d5
liang shan guang <1648266192@qq.com>
2019-07-16 00:18:20 +0800

    Confirm消息确认机制的实现

---
+++ RabbitMQ/RabbitMQ消息中间件技术精讲/第3章_RabbitMQ高级特性/code/rabbitmqdemo/src/main/java/com/huawei/l00379880/rabbitmqdemo/confirm/Consumer.java
@@ -0,0 +1,10 @@
+/***********************************************************
+ * @Description : 
+ * @author      : 梁山广(Laing Shan Guang)
+ * @date        : 2019-07-16 00:13
+ * @email       : liangshanguang2@gmail.com
+ ***********************************************************/
+package com.huawei.l00379880.rabbitmqdemo.confirm;
+
+public class Consumer {
+}
