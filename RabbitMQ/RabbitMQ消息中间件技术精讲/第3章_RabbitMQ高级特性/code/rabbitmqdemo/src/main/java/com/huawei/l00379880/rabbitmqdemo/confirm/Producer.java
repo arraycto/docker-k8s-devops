01af1f9d7f37c4f0b8094010907dfa0166e22885
liang shan guang <1648266192@qq.com>
2019-07-16 08:02:19 +0800

    消息消费不了

--- RabbitMQ/RabbitMQ消息中间件技术精讲/第3章_RabbitMQ高级特性/code/rabbitmqdemo/src/main/java/com/huawei/l00379880/rabbitmqdemo/confirm/Producer.java
+++ RabbitMQ/RabbitMQ消息中间件技术精讲/第3章_RabbitMQ高级特性/code/rabbitmqdemo/src/main/java/com/huawei/l00379880/rabbitmqdemo/confirm/Producer.java
@@ -6,5 +6,57 @@
  ***********************************************************/
 package com.huawei.l00379880.rabbitmqdemo.confirm;
 
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.ConfirmListener;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
 public class Producer {
+    public static void main(String[] args) throws IOException, TimeoutException {
+        // 1.ConnectionFactory:获取连接工厂,并进行基础配置
+        ConnectionFactory connectionFactory = new ConnectionFactory();
+        connectionFactory.setHost("122.112.151.149");
+        connectionFactory.setPort(5672);
+        connectionFactory.setVirtualHost("/");
+        connectionFactory.setUsername("root");
+        connectionFactory.setPassword("aA111111");
+
+
+        // 2.Connection:通过连接工厂创建一个连接
+        Connection connection = connectionFactory.newConnection();
+
+        // 3.Channel:通过Connection创建Channel,连接后创建需要的数据通信信道，可发送和接收消息
+        Channel channel = connection.createChannel();
+
+        // 4.指定我们的消息投递模式：消息的确认模式
+        channel.confirmSelect();
+
+        String exchangeName = "test_confirm_exchange";
+        String routingKey = "confirm.save";
+
+        // 5.发送一条消息
+        String msg = "RabbitMQ Confirm Message";
+        channel.basicPublish(exchangeName,routingKey,null, msg.getBytes());
+
+        // 6.添加一个确认监听
+        channel.addConfirmListener(new ConfirmListener() {
+            @Override
+            public void handleAck(long deliveryTag, boolean multiple) throws IOException {
+                // 返回失败时
+                System.out.println("-------no ack------");
+            }
+
+            @Override
+            public void handleNack(long deliveryTag, boolean multiple) throws IOException {
+                // 返回成功时
+                System.out.println("---------ack-------");
+            }
+        });
+
+//        channel.close();
+//        connection.close();
+    }
 }
